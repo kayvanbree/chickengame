66f9dcdeee627c202f1ff746e6c97eea007f289b
Vera Mommersteeg <v.mommersteeg@innovattic.com>
2017-01-20 22:51:28 +0100

    Added initial files for game

---
+++ ChickenGame/Assets/Scripts/Player.cs
@@ -0,0 +1,163 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class Player : MonoBehaviour
+{
+	public string Name = "PlayerNone";
+	public int playerIdx = -1;
+
+	public x360_Gamepad gamepad;
+
+	public AudioClip clipButtonA;
+	public AudioClip clipButtonB;
+	public AudioClip clipButtonX;
+	public AudioClip clipButtonY;
+	public AudioClip clipTriggerRight;
+	public AudioClip clipTriggerLeft;
+
+	private AudioSource audioSource;
+
+	public int[] buttonIndices = new int[6];
+	public int currentButtonIndex = -1;
+
+	// Use this for initialization
+	void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	// Update is called once per frame
+	public void Update()
+	{
+		HandleInput();
+	}
+
+	private void HandleInput()
+	{
+		// reset index to -1, so we know that this player is not pressing any buttons
+		if (gamepad.GetButtonUp("A"))
+		{
+			//Debug.Log("a is released");
+			currentButtonIndex = -1;
+		}
+
+		if (gamepad.GetButtonUp("B"))
+			currentButtonIndex = -1;
+
+		if (gamepad.GetButtonUp("X"))
+			currentButtonIndex = -1;
+
+		if (gamepad.GetButtonUp("Y"))
+			currentButtonIndex = -1;
+
+		if (gamepad.GetButtonDown("A"))
+		{
+			// play sound
+			if (!audioSource.isPlaying)
+			{	
+				audioSource.clip = clipButtonA;
+				audioSource.Play();
+			}
+			// get the index for this button for this player and set it to the current button index
+			// this way we can check if this is the same as the one on the screen
+			currentButtonIndex = GetButtonIndex("A");
+		}
+		if (gamepad.GetButtonDown("B"))
+		{
+			// play sound
+			if (!audioSource.isPlaying)
+			{
+				audioSource.clip = clipButtonB;
+				audioSource.Play();
+				currentButtonIndex = GetButtonIndex("B");
+			}
+		}
+		if (gamepad.GetButtonDown("X"))
+		{
+			// play sound
+			if (!audioSource.isPlaying)
+			{
+				audioSource.clip = clipButtonX;
+				audioSource.Play();
+				currentButtonIndex = GetButtonIndex("X");
+			}
+		}
+		if (gamepad.GetButtonDown("Y"))
+		{
+			// play sound
+			if (!audioSource.isPlaying)
+			{
+				audioSource.clip = clipButtonY;
+				audioSource.Play();
+				currentButtonIndex = GetButtonIndex("Y");
+			}
+		}
+		if(gamepad.GetTriggerTap_L())
+		{
+			// play sound
+			if (!audioSource.isPlaying)
+			{
+				audioSource.clip = clipTriggerLeft;
+				audioSource.Play();
+				currentButtonIndex = GetButtonIndex("LT");
+			}
+		}
+		if (gamepad.GetTriggerTap_R())
+		{
+			// play sound
+			if (!audioSource.isPlaying)
+			{
+				audioSource.clip = clipTriggerRight;
+				audioSource.Play();
+				currentButtonIndex = GetButtonIndex("RT");
+			}
+		}
+	}
+
+	public int GetButtonIndex(string button)
+	{
+		int index = -1;
+
+		switch (button)
+		{
+			case "A":
+				{
+					index = buttonIndices[0];
+				}
+				break;
+			case "B":
+				{
+					index = buttonIndices[1];
+				}
+				break;
+			case "X":
+				{
+					index = buttonIndices[2];
+				}
+				break;
+			case "Y":
+				{
+					index = buttonIndices[3];
+				}
+				break;
+			case "LT":
+				{
+					index = buttonIndices[4];
+				}
+				break;
+			case "RT":
+				{
+					index = buttonIndices[5];
+				}
+				break;
+		}
+
+		return index;
+	}
+
+	void OnGUI()
+	{
+		
+	}
+}
---
+++ ChickenGame/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerManager : MonoBehaviour
+{
+	public GameObject PlayerPrefab;
+
+	private List<GameObject> Players;
+	private int[] buttonIndices = new int[24];
+	private int numberPlayers = -1;
+
+	// Use this for initialization
+	void Start()
+	{
+		numberPlayers = GamepadManager.Instance.GamepadCount;
+		for (int i = 0; i < 24; i++)
+		{
+			buttonIndices[i] = i;
+		}
+
+		Players = new System.Collections.Generic.List<GameObject>(numberPlayers);
+
+		for (int i = 0; i < numberPlayers; i++)
+		{
+			// instantiate player on position (later use to spawn at real pos)
+			GameObject player = Instantiate(PlayerPrefab, new Vector3(0.0f, 0.0f, 0.0f), PlayerPrefab.transform.rotation) as GameObject;
+
+			player.GetComponent<Player>().gamepad = GamepadManager.Instance.GetGamepad(i + 1);
+
+			player.GetComponent<Player>().playerIdx = i;
+
+			// set the indices for the buttons, this way we can later on check if the button index was the same as some other action
+			for (int j = 0; j < 6; j++)
+				player.GetComponent<Player>().buttonIndices[j] = buttonIndices[i * 6 + j];
+
+			// set random player properties
+			player.GetComponent<Player>().Name = "Player" + i;
+
+			// add to player list
+			Players.Add(player);
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		for (int i = 0; i < numberPlayers; i++)
+		{
+			Players[i].GetComponent<Player>().Update();
+
+			// check if there are any items left in the sequence
+			// if the button pressed for this specific player is equal to the sound in the list
+			/*if (Players[i].GetComponent<Player>().currentButtonIndex == some base of a player)
+                {
+                    Players[i].GetComponent<Player>().currentButtonIndex = -1;
+
+                    Debug.Log("player idx: " + Players[i].ToString() + "sound nr: " );
+                   
+                    // we know that that player pressed the correct button for that image
+                    ScreenImages.RemoveAt(0);
+                }
+                else if (Players[i].GetComponent<Player>().currentButtonIndex > -1)
+                {
+                    Players[i].GetComponent<Player>().currentButtonIndex = -1;
+                }
+            }*/
+		}
+	}
+
+
+	void OnGUI()
+	{
+
+	}
+}
