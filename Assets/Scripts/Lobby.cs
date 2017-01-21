5f0bd05115f0b45d2b1eb1197397622fa9f52faf
Kay van Bree <Kay van Bree>
2017-01-21 23:17:34 +0100

    Added loading scene

--- Assets/Scripts/Lobby.cs
+++ Assets/Scripts/Lobby.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Lobby : MonoBehaviour
 {
@@ -19,7 +20,7 @@ public class Lobby : MonoBehaviour
 
     public void StartGame()
     {
-        Debug.Log("Starting new game");
+        SceneManager.LoadScene("Game");
     }
 
     public void AddPlayer(int index)
