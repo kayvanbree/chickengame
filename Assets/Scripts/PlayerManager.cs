347bf9019fedb6e44d30a772b6399c5186cbde8b
Kay van Bree <Kay van Bree>
2017-01-21 16:17:34 +0100

    Can add players now

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -10,7 +10,7 @@ public class PlayerManager : MonoBehaviour
 	public GameObject YellowBarn;
 	public GameObject BlueBarn;
 
-	private List<GameObject> Players;
+	private GameObject[] Players;
 	private int[] buttonIndices = new int[24];
 	private int numberPlayers = -1;
 
@@ -23,42 +23,65 @@ public class PlayerManager : MonoBehaviour
 			buttonIndices[i] = i;
 		}
 
-		Players = new System.Collections.Generic.List<GameObject>(numberPlayers);
-
-		for (int i = 0; i < numberPlayers; i++)
-		{
-			// instantiate player on position (later use to spawn at real pos)
-			GameObject player = Instantiate(PlayerPrefab, new Vector3(0.0f, 0.0f, 0.0f), PlayerPrefab.transform.rotation) as GameObject;
-
-			player.GetComponent<Player>().gamepad = GamepadManager.Instance.GetGamepad(i + 1);
-
-			player.GetComponent<Player>().playerIdx = i;
-
-			player.GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonUserControl>().playerIdx = (i + 1);
-
-			// set the indices for the buttons, this way we can later on check if the button index was the same as some other action
-			for (int j = 0; j < 6; j++)
-				player.GetComponent<Player>().buttonIndices[j] = buttonIndices[i * 6 + j];
-
-			// set player tag
-			player.tag = "Player" + i;
-
-			// add to player list
-			Players.Add(player);
-		}
-
-		Players[0].GetComponent<Player>().playerColor = new Color(0, 1, 0); //green
-		Players[0].GetComponent<Player>().barnPrefab = GreenBarn;
-
-		Players[1].GetComponent<Player>().playerColor = new Color(1, 0, 0); //red
-		Players[1].GetComponent<Player>().barnPrefab = RedBarn;
-
-		Players[2].GetComponent<Player>().playerColor = new Color(1, 1, 0); //yellow
-		Players[2].GetComponent<Player>().barnPrefab = YellowBarn;
-
-		Players[3].GetComponent<Player>().playerColor = new Color(0, 0, 1); //blue
-		Players[3].GetComponent<Player>().barnPrefab = BlueBarn;
+        Players = new GameObject[numberPlayers];
 	}
+
+    /// <summary>
+    /// Adds a player to the game on the given index
+    /// </summary>
+    /// <param name="index"></param>
+    void AddPlayer(int index)
+    {
+        // instantiate player on position (later use to spawn at real pos)
+        GameObject player = Instantiate(PlayerPrefab, new Vector3(0.0f, 0.0f, 0.0f), PlayerPrefab.transform.rotation) as GameObject;
+
+        player.GetComponent<Player>().gamepad = GamepadManager.Instance.GetGamepad(index + 1);
+
+        player.GetComponent<Player>().playerIdx = index;
+
+        player.GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonUserControl>().playerIdx = (index + 1);
+
+        // set the indices for the buttons, this way we can later on check if the button index was the same as some other action
+        for (int j = 0; j < 6; j++)
+            player.GetComponent<Player>().buttonIndices[j] = buttonIndices[index * 6 + j];
+
+        // set player tag
+        player.tag = "Player" + index;
+
+        // add to player list
+        Players[index] = player;
+
+        // Get correct player color and barn
+        switch(index)
+        {
+            case 0:
+            {
+                Players[0].GetComponent<Player>().playerColor = new Color(0, 1, 0); //green
+                Players[0].GetComponent<Player>().barnPrefab = GreenBarn;
+                break;
+            }
+            case 1:
+            {
+                Players[1].GetComponent<Player>().playerColor = new Color(1, 0, 0); //red
+                Players[1].GetComponent<Player>().barnPrefab = RedBarn;
+                break;
+            }
+            case 2:
+            {
+                Players[2].GetComponent<Player>().playerColor = new Color(1, 1, 0); //yellow
+                Players[2].GetComponent<Player>().barnPrefab = YellowBarn;
+                break;
+            }
+            case 3:
+            {
+                Players[3].GetComponent<Player>().playerColor = new Color(0, 0, 1); //blue
+                Players[3].GetComponent<Player>().barnPrefab = BlueBarn;
+                break;
+            }
+        }
+    }
+
+    
 
 	void SendToBarn()
 	{
@@ -68,12 +91,14 @@ public class PlayerManager : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+        CheckForNewPlayers();
+
 		for (int i = 0; i < numberPlayers; i++)
 		{
 			Players[i].GetComponent<Player>().Update();
 		}
 
-		for(int i = 0; i < Players.Count; i++)
+		for(int i = 0; i < Players.Length; i++)
 		{
 			Player currentPlayer = Players[i].GetComponent<Player>();
 			//if a player pressed a button
@@ -90,6 +115,21 @@ public class PlayerManager : MonoBehaviour
 			}
 		}
 	}
+
+    void CheckForNewPlayers()
+    {
+        for (int i = 0; i < numberPlayers; i++)
+        {
+            if (Players[i] != null) continue;
+
+            x360_Gamepad pad = GamepadManager.Instance.GetGamepad(i + 1);
+            bool pressed = pad.GetButton("A");
+            if (pressed)
+            {
+                AddPlayer(i);
+            }
+        }
+    }
 
 	void OnGUI()
 	{
