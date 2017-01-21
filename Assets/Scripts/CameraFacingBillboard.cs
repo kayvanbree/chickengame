85321948159d18e4c7d621891e654ced2a530568
Kay van Bree <Kay van Bree>
2017-01-21 11:56:23 +0100

    Forgot the billboard script

--- Assets/Scripts/CameraFacingBillboard.cs
+++ Assets/Scripts/CameraFacingBillboard.cs
@@ -1,16 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class CameraFacingBillboard : MonoBehaviour {
+public class CameraFacingBillboard : MonoBehaviour
+{
+    public Camera m_Camera;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Update()
+    {
+        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+            m_Camera.transform.rotation * Vector3.up);
+    }
 }
