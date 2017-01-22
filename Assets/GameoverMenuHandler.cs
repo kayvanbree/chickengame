98fc8e3608eaed155dc5d780307654c3b411d33f
Kay van Bree <Kay van Bree>
2017-01-22 03:30:51 +0100

    Added GameOver menu

---
+++ Assets/GameoverMenuHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameoverMenuHandler : MonoBehaviour
+{
+    void Update()
+    {
+        CheckForStartPressed();
+    }
+
+    void CheckForStartPressed()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            x360_Gamepad pad = GamepadManager.Instance.GetGamepad(i + 1);
+            if (!pad.IsConnected) continue;
+            bool pressed = pad.GetButton("Start");
+            if (pressed)
+            {
+                SceneManager.LoadScene("MainMenu");
+                return;
+            }
+        }
+    }
+}
