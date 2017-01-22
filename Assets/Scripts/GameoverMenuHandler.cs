2af50ceaf32803fde0fb3a86c8a20da1e60ca5f3
Kay van Bree <Kay van Bree>
2017-01-22 14:12:40 +0100

    Chicken light now color of winning player

--- Assets/Scripts/GameoverMenuHandler.cs
+++ Assets/Scripts/GameoverMenuHandler.cs
@@ -17,6 +17,12 @@ public class GameoverMenuHandler : MonoBehaviour
 
     public Image PlayerWonImage;
 
+    public MeshRenderer DishMesh;
+    public MeshRenderer BarnMesh;
+    public SkinnedMeshRenderer ChickenMesh;
+
+    Color playerColor;
+
     void Start()
     {
         GameStateManager = FindObjectOfType<GameStateManager>();
@@ -27,16 +33,48 @@ public class GameoverMenuHandler : MonoBehaviour
         {
             case 0:
                 PlayerWonImage.sprite = GreenWon;
+                playerColor = Color.green;
                 break;
             case 1:
                 PlayerWonImage.sprite = RedWon;
+                playerColor = Color.red;
                 break;
             case 2:
                 PlayerWonImage.sprite = YellowWon;
+                playerColor = Color.yellow;
                 break;
             case 3:
                 PlayerWonImage.sprite = BlueWon;
+                playerColor = Color.blue;
                 break;
+        }
+
+        for (int i = 0; i < DishMesh.materials.Length; i++)
+        {
+            if (DishMesh.materials[i].name == "GreenFarmColorMaterial (Instance)")
+            {
+                Material mat = DishMesh.materials[i];
+                mat.SetColor("_Color", playerColor);
+                DishMesh.materials[i] = mat;
+            }
+        }
+        for (int i = 0; i < BarnMesh.materials.Length; i++)
+        {
+            if (BarnMesh.materials[i].name == "GreenFarmColorMaterial (Instance)")
+            {
+                Material mat = BarnMesh.materials[i];
+                mat.SetColor("_Color", playerColor);
+                BarnMesh.materials[i] = mat;
+            }
+        }
+        for (int i = 0; i < ChickenMesh.materials.Length; i++)
+        {
+            if (ChickenMesh.materials[i].name == "Mat_PlayerOwner (Instance)")
+            {
+                Material mat = ChickenMesh.materials[i];
+                mat.SetColor("_Color", playerColor);
+                ChickenMesh.materials[i] = mat;
+            }
         }
     }
 
